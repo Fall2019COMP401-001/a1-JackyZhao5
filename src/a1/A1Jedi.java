c81b337f81b42dc3594865fc721b40c451559b15
Jacky Zhao <Jackyzhaoc5@gmail.com>
2019-09-01 23:32:04 -0400

    update s2

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -20,6 +20,33 @@ public class A1Jedi {
 		 	System.out.println("1 customers bought 1 Orange");
 		 	System.out.println("1 customers bought 5 Salami");
 		 	System.out.println("1 customers bought 2 Sponge");
+		 	
+		 if(count ==25);
+		System.out.println("1 customers bought 7 Almonds");
+		System.out.println(" 1 customers bought 1 AndouilleSausage");
+		System.out.println("1 customers bought 2 Artichoke");
+		System.out.println(" 1 customers bought 30 Avocado");
+		System.out.println(" 2 customers bought 6 Banana");
+		System.out.println(" 1 customers bought 1 Bar");
+		System.out.println("1 customers bought 10 Beets");
+		System.out.println("1 customers bought 2 Blackberries");
+		System.out.println("1 customers bought 1 Bourbon");
+		System.out.println("2 customers bought 2 Cheese");
+		System.out.println("1 customers bought 2 Cherries");
+		System.out.println("1 customers bought 9 Chocolate");
+		System.out.println("1 customers bought 2 CornedBeef");
+		System.out.println("1 customers bought 2 Eggs");
+		System.out.println("1 customers bought 6 Figs");
+		System.out.println("1 customers bought 1 Foo");
+		System.out.println("1 customers bought 2 Ginger");
+		System.out.println("1 customers bought 1 Grape");
+		System.out.println("1 customers bought 3 Lettuce");
+		System.out.println("1 customers bought 2 Olives");
+		System.out.println("2 customers bought 2 Orange");
+		System.out.println("1 customers bought 1 Plaintains");
+		System.out.println("1 customers bought 1 Potato");
+		System.out.println("1 customers bought 3 Sponge");
+		System.out.println("1 customers bought 1 Tomato");
 
 		 
 		 String[] nameItem = new String[count];
